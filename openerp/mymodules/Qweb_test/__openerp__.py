e2f08cb40f203801255e03e7d2950680bb581b3f
Kevin <kfx2007@outlook.com>
2014-07-24 08:53:41 +0800

    I'm the commiter

---
+++ openerp/mymodules/Qweb_test/__openerp__.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8 -*-
+
+{
+        'name':'Web Example',
+        'description':'Basic Example of a web module',
+        'category':'Hidden',
+        'depends':['web'],
+        'js':['static/js/first_module.js'],
+        'data':['static/xml/web_example.xml'],
+        'css':['static/css/web_example.css'],
+        'qweb':['static/xml/qweb_example.xml'],
+
+}
